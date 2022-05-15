3d764c3f0ea81abb2184e70c6982f1276daecb35
Qervas <djmax96945147@outlook.com>
2022-05-14 23:30:28 -0700

    Allzeroes_v6 eg1
    situations of WaitForSingleObject mutex from 0 to INFINITE.  Mutex ownership transfer

--- MultiThreads-WINAPI/Allzeroes_v6_eg1.cpp
+++ MultiThreads-WINAPI/Allzeroes_v6_eg1.cpp
@@ -3,11 +3,15 @@
 using std::cout;
 using std::endl;
 
+#define KERNEL_OWNS_MUTEX
+#define ABANDON
 /*About the Mutex Kernel Object
 * Mutex -> Mutual exclusion(no two threads can access the same resource at the same time)
 *
 * Recursion counter -> how many times a thread has acquired the mutex kernel object
 *
+* Ownership of Mutex: The thread which is the owner of the thread, if that comes to end
+* then the mutex kernel object will be destroyed by the kernel
 * Launch only one instance of the application using mutex App.exe
 *
 *
@@ -15,7 +19,12 @@ using std::endl;
 HANDLE hMutex = NULL;
 
 int main() {
-	hMutex = CreateMutex(NULL, FALSE, TEXT("M1"));
+#ifdef KERNEL_OWNS_MUTEX
+	hMutex = CreateMutex(NULL, FALSE, TEXT("M1"));//mutex belongs to kernel
+#else
+	hMutex = CreateMutex(NULL, TRUE, TEXT("M1"));//mutex belongs to the thread as its HANDLE referring
+#endif // KERNEL_OWNS_MUTEX
+
 	int err = GetLastError();
 	if (err != 0 && err != ERROR_ALREADY_EXISTS) {
 		cout << endl << "The mutex initalization failed ...";
@@ -26,11 +35,24 @@ int main() {
 	if it is signaled but if not siganled, then it will wait till the time specified
 	if it is acquired by the thread, then after acquiring it, it will make it
 	non siagnled at the same time which means than no other thread can now acquire it*/
-	DWORD dw = WaitForSingleObject(hMutex, 0);
-	if (dw == WAIT_TIMEOUT) {
-		cout << endl << "One instance of the application is already running...";
+#ifdef  ABANDON
+	DWORD dw = WaitForSingleObject(hMutex, INFINITE);
+
+	if (dw == WAIT_ABANDONED) {
+		cout << endl << "Wait abandoned on mutex";
 		return EXIT_FAILURE;
 	}
+#else
+
+	DWORD dw = WaitForSingleObject(hMutex, 0);
+
+	if (dw == WAIT_TIMEOUT) {
+		cout << endl << "one instance of event kernel object is running...";
+		return EXIT_FAILURE;
+	}
+
+#endif //  ABANDON
+
 
 	if (dw == WAIT_ABANDONED || dw == WAIT_FAILED || dw == WAIT_TIMEOUT) {
 		cout << endl << "The wait failed";
