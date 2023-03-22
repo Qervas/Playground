7344f066e3c25946591010f2a58f73fe0cca4ecf
Qervas <djmax96945147@outlook.com>
2023-03-22 16:13:34 +0800

    added specular maps

---
+++ Computer_Graphics/OpenGL/src/6_specular_maps/VAO.cpp
@@ -0,0 +1,24 @@
+#include "6_specular_maps/VAO.h"
+
+VAO::VAO(){
+	glGenVertexArrays(1, &ID);
+}
+
+void VAO::LinkAttrib(VBO& VBO, GLuint layout, GLuint numComponents, GLenum type, GLsizeiptr stride, void* offest){
+	VBO.Bind();
+	glVertexAttribPointer(layout, numComponents, type, GL_FALSE, stride, offest);
+	glEnableVertexAttribArray(layout);
+	VBO.Unbind();
+}
+
+void VAO::Bind(){
+	glBindVertexArray(ID);
+}
+
+void VAO::Unbind(){
+	glBindVertexArray(0);
+}
+
+void VAO::Delete(){
+	glDeleteVertexArrays(1, &ID);
+}
