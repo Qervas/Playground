606465e3053f08de9f68632dab2f46d1ebb206db
Qervas <djmax96945147@outlook.com>
2023-03-22 16:07:43 +0800

    spotlight, direct light, point light in lighting shader

---
+++ Computer_Graphics/OpenGL/include/7_types_of_light/EBO.h
@@ -0,0 +1,16 @@
+#ifndef EBO_CLASS_H
+#define EBO_CLASS_H
+
+#include <glad.h>
+
+class EBO{
+	public:
+	GLuint ID;
+	EBO(GLuint* indices, GLsizeiptr size);
+
+	void Bind();
+	void Unbind();
+	void Delete();
+};
+
+#endif
