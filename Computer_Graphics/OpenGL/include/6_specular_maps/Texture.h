67681e4ac0db603a4616ae9217f439357eb70e60
Qervas <djmax96945147@outlook.com>
2023-03-22 16:07:00 +0800

    specular maps for lighting

---
+++ Computer_Graphics/OpenGL/include/6_specular_maps/Texture.h
@@ -0,0 +1,23 @@
+#ifndef TEXTURE_CLASS_H
+#define TEXTURE_CLASS_H
+
+#include <glad.h>
+#include <stb/stb_image.h>
+
+#include "6_specular_maps/shaderClass.h"
+
+class Texture{
+	public:
+		GLuint ID;
+		GLenum type;
+		GLuint unit;
+		Texture(const char* image, GLenum texType, GLuint slot, GLenum format, GLenum pixelType);
+
+		void texUnit(Shader& shader, const char* uniform, GLuint unit);
+		void Bind();
+		void Unbind();
+		void Delete();
+
+};
+
+#endif
