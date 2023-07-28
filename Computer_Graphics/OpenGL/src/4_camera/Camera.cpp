4ed6f6e94ef607f0b0ac2d10e13892c3a419f5d3
FrankYin <djmax96945147@outlook.com>
2023-07-28 17:40:42 +0800

    4: speed fixed

--- Computer_Graphics/OpenGL/src/4_camera/Camera.cpp
+++ Computer_Graphics/OpenGL/src/4_camera/Camera.cpp
@@ -38,11 +38,11 @@ void Camera::Inputs(GLFWwindow* window){
 	}
 	if (glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS)
 	{
-		speed = 0.4f;
+		speed = 0.004f;
 	}
 	else if (glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_RELEASE)
 	{
-		speed = 0.1f;
+		speed = 0.001f;
 	}
 
 	//mouse
