8f06435b60adceefe16c586358a54dfc700d9a4d
Qervas <djmax96945147@outlook.com>
2023-12-04 18:33:22 +0100

    camera class added

--- Computer_Graphics/RayTracing/main.cpp
+++ Computer_Graphics/RayTracing/main.cpp
@@ -1,5 +1,6 @@
 #include "helper.h"
 #include "color.h"
+#include "camera.h"
 #include "hittable.h"
 #include "hittable_list.h"
 #include "sphere.h"
@@ -7,50 +8,17 @@
 #include <fstream> // Include the fstream header
 #include <random>
 
-color ray_color(const ray& r, const hittable& world){
-	hit_record rec;
-	if(world.hit(r, interval(0, infinity), rec)){
-		return 0.5 * (rec.normal + color(1.0,1.0,1.0));
-	}//normal_vec [-1,1]
-		
-	vec3 unit_direction = unit_vector(r.direction());
-	auto a = 0.5*(unit_direction.y() + 1.0);
-	return (1.0-a)*color(1.0, 1.0, 1.0) + a*color(0.5, 0.7, 1.0);
-	
-}
-
 
 
 int main() {
-    // Image
-	auto aspect_ratio = 16.0 / 9.0;
-	int image_width = 1800;
-	int image_height = static_cast<int>(image_width/ aspect_ratio);
 
-	//Camera
-	auto focal_length = 1.0;
-	auto viewport_height = 2.0;
-	auto viewport_width = viewport_height * (static_cast<double>(image_width)/image_height);
-	auto camera_center = point3(0,0,0);
+
 
 	//World
 	hittable_list world;
 	world.add(make_shared<sphere>(point3(0.0,0.0,-1.0), 0.5));
 	world.add(make_shared<sphere>(point3(0.0, -100.5, -1.0), 100));
 	
-	//viewport vectrors along the viewport edges
-	auto viewport_u = vec3(viewport_width, 0,0);
-	auto viewport_v = vec3(0, -viewport_height, 0);
-
-	//delta vectors of pixel
-	auto pixel_delta_u = viewport_u / image_width;
-	auto pixel_delta_v = viewport_v / image_height;
-
-	//upper left pixel location
-	auto viewport_upper_left = camera_center - vec3(0,0,focal_length) - viewport_u/2 - viewport_v/2;
-	auto pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
-
-	//Render
 
     std::random_device rd;
     std::mt19937 gen(rd());
@@ -66,22 +34,12 @@ int main() {
         return 1;
     }
 
-    // Write the P3 PPM header
-    ppmFile << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+	camera cam(ppmFile);
 
-    for (int j = 0; j < image_height; ++j) {
-        std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
-        for (int i = 0; i < image_width; ++i) {
-            auto pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
-			auto ray_direction = pixel_center - camera_center;
-			ray r(camera_center, ray_direction);
-
-			color pixel_color = ray_color(r, world);
-            write_color(ppmFile, pixel_color);
-        }
-    }
-    std::clog << "\rDone.                 \n";
-
+	
+    cam.aspect_ratio = 16.0 / 9.0;
+    cam.image_width  = 1000;
+	cam.render(world);
     // Close the file when done
     ppmFile.close();
 
